13cf6b7d3e475690592e2066805d8a8e792f7595
Carlos P Navarrete Filho <karnav@gmail.com>
2017-01-27 16:56:17 -0200

    Add admin tools for editing the cats

--- js/app.js
+++ js/app.js
@@ -19,6 +19,20 @@
     setCurrentCat: function(cat){
       model.currentCat = cat;
     },
+    incrementCurrentCat: function(){
+      model.currentCat.counter++;
+    },
+    editCurrentCat: function(cat){
+      if (cat.name)
+        model.currentCat.name = cat.name;
+      if (cat.src)
+        model.currentCat.src = cat.src;
+      if (cat.counter)
+        model.currentCat.counter = cat.counter;
+    },
+    populateAdminTool: function(){
+
+    },
     renderCat: function(){
       view.displayView.render(this.getCurrentCat());
     },
@@ -62,30 +76,6 @@
     }
   }
 
-  // var controller = {
-  //   getCat: function(index){
-  //     return model.cats[index];
-  //   },
-  //   getCats: function(){
-  //     return model.cats;
-  //   },
-  //   getCurrentCat: function(){
-  //     return model.currentCat;
-  //   },
-  //   setCurrentCat: function(cat){
-  //     model.currentCat = cat;
-  //   },
-  //   renderCat: function(){
-  //     var cat = getCurrentCat();
-  //     console.log(cat);
-  //     view.displayView.render(getCurrentcat());
-  //   },
-  //   init: function(){
-  //     model.init();
-  //     view.init(this.getCats());
-  //   }
-  // }
-
   var view = {
     listView: {
       target: document.getElementById('cats-list'),
@@ -96,7 +86,11 @@
                cat = cats[index];
            catNode.textContent = cat.name;
            catNode.addEventListener('click', function(){
+             var visible = view.adminView.tool.classList;
+             if (visible.contains('hidden'))
+              visible.remove('hidden');
              controller.setCurrentCat(cat);
+             view.adminView.populate(cat);
              controller.renderCat();
            });
            self.target.appendChild(catNode);
@@ -106,29 +100,83 @@
     displayView: {
       targetTitle: document.getElementById('cat-title'),
       targetImg: document.getElementById('cat-img'),
+      render: function(cat, mode){
+        if (mode === undefined || mode === 'full-render'){
+          this.targetImg.setAttribute('src', cat.src);
+        }
+        this.targetTitle.textContent = cat.name + ': ' + cat.counter;
+      }
+    },
+    adminView: {
+      tool: document.getElementById('admin-tools'),
+      controls: document.getElementById('admin-form'),
+      button: document.getElementById('admin-button'),
+      cancel: document.getElementById('cancel-button'),
+      save: document.getElementById('save-button'),
+      populate: function(){
+        var currentCat = controller.getCurrentCat(),
+            catNameTxt = document.getElementById('admin-name'),
+            catSrcTxt = document.getElementById('admin-url'),
+            catClickTxt = document.getElementById('admin-click');
+        catNameTxt.value = currentCat.name;
+        catSrcTxt.value = currentCat.src;
+        catClickTxt.value = currentCat.counter;      
+      },
       render: function(cat){
-        this.targetTitle.textContent = cat.name;
-        this.targetImg.setAttribute('src', cat.src);
-      //   var target = view.displayView.target,
-      //       title = document.createElement('h3');
-      //   redrawTitle();
-      //   var img = document.createElement('img');
-      //   img.setAttribute('src', cat.src);
-      //   img.setAttribute('alt', cat.name + ' the cat.');
-      //   img.addEventListener('click', function(){
-      //     cat.counter++;
-      //     redrawTitle();
-      //   });
-      //   target.appendChild(title);
-      //   target.appendChild(img);
-      //   function redrawTitle(){
-      //     title.textContent = cat.name + ': ' + cat.counter;
-      //   }
+        this.controls.classList.toggle('hidden');
+      }
+    },
+    clearElements: function(elements){
+      for (var i = 0;  i < elements.length; i++){
+        elements[i].value = "";
       }
     },
     init: function(cats){
-      // this.listView.render.call(view.displayView, cats);
-      this.listView.render(cats);
+
+      var targetImg = this.displayView.targetImg,
+          targetTitle = this.displayView.targetTitle,
+          displayView = this.displayView,
+          adminView = this.adminView,
+          adminButton = this.adminView.button,
+          adminTool = this.adminView.tool,
+          saveButton = this.adminView.save,
+          cancelButton = this.adminView.cancel;
+      
+     this.listView.render(cats);
+
+     //Registering Event Listeners.
+      adminButton.addEventListener('click', function(){
+        adminView.render();
+      });
+
+      cancelButton.addEventListener('click', function(e){
+        var currentCat = controller.getCurrentCat();
+        view.adminView.populate(currentCat);
+        adminView.render();
+        e.preventDefault();
+      });
+
+      saveButton.addEventListener('click', function(e){
+        var catName = document.getElementById('admin-name').value, 
+            catSrc = document.getElementById('admin-url').value, 
+            catCounter = document.getElementById('admin-click').value, 
+            cat = new Cat({name: catName, src: catSrc, counter: catCounter }),
+            currentCat;
+        controller.editCurrentCat(cat);
+        currentCat = controller.getCurrentCat();
+        adminView.populate(currentCat);
+        displayView.render(currentCat, "full-render");
+        adminView.render();
+        e.preventDefault();
+      });
+
+      targetImg.addEventListener('click', function(){
+        var currentCat = controller.getCurrentCat();
+        if (currentCat !== undefined){
+          controller.incrementCurrentCat();
+          displayView.render(currentCat);
+        }
+      });
     }
   }
   var controller = new Controller();
